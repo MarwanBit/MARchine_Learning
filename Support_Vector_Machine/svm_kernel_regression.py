ec3a11020b223c95d74525677e0f84fe29d85535
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2023-07-08 20:25:40 -0400

    Created the file for regression based SVM

---
+++ Support_Vector_Machine/svm_kernel_regression.py
@@ -0,0 +1,6 @@
+class SVM_Regression():
+    def __init__(self):
+        '''
+        This class implements the SVM for regression, which is discuessed in Neural Networks and Learning Machines Book.
+        '''
+        pass
